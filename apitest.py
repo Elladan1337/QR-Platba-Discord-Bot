1817aea111b057a974731bb5f7ec334d5b8f18d5
Martin Košatka <82899462+Elladan1337@users.noreply.github.com>
2023-02-08 11:53:31 +0100

    Created test for API, success!

---
+++ apitest.py
@@ -0,0 +1,25 @@
+import requests
+
+url = "http://api.paylibo.com/paylibo/generator/czech/image"
+params = {
+	'accountPrefix':'51',
+	'accountNumber':'9056360217',
+	'bankCode':'0100',
+	'amount':600,
+	'currency':'CZK',
+	'vs':'',
+	'ks':'',
+	'ss':'',
+	'identifier':'',
+	'date':'2023-01-01',
+	'message':'ahoj',
+	'compress':False,
+	'branding':False,
+	'size':400
+}
+
+response = requests.get(url = url, params = params, stream=True)
+print(response.status_code)
+if response.status_code == 200:
+	with open('img.png', 'wb') as out_file:
+		out_file.write(response.content)
--- bot.py
+++ bot.py
@@ -16,4 +16,14 @@ client = commands.Bot(command_prefix = "!", intents = intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+client.run(TOKEN
+
+
+
+### !qrcreate command
+### Looks at last ten messages
+### Does a Regex-check for the bank account number
+### Does a Regex-check for the payment number
+### Submits this to the SPAYD API https://qr-platba.cz/pro-vyvojare/restful-api/
+### Receive .png file
+### Post .png file into chat.
